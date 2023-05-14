903e228a04c8ca7cd583e8652f9f6bf7b9819873
Shidil Eringa <shidil@live.com>
2023-05-14 19:01:44 +1000

    chore: improved fish fn search and added reload

--- .config/fish/functions/search.fish
+++ .config/fish/functions/search.fish
@@ -1,13 +1,19 @@
 function search
-  ag -l $argv[1] | fzf --preview 'bat --style numbers,changes --color=always {} | head -500'
+	# ag -l $argv[1] | fzf --preview 'bat --style numbers,changes --color=always {} | head -500'
+   rg --line-number --no-heading --color=always --smart-case "$argv" | fzf -d ':' -n 2.. --ansi --no-sort --preview-window 'down:20%:+{2}' --preview 'bat --style=numbers --color=always --highlight-line {2} {1}'
 end
 
 function search_run
-  search $argv[1] | xargs $argv[2]
+  # extract file name only, cut line number and text
+  search $argv[1] |  cut -f1,1 -d':' | xargs $argv[2]
 end
 
 function search_vi
-  search_run $argv[1] nvim
+  set out $(search $argv)
+  set file $(echo $out | cut -f1,1 -d':')
+  set line $(echo $out | cut -f1,2 -d':' | rev | cut -f1,1 -d':' | rev)
+  echo $file +"normal $(echo $line)Gzz"
+  nvim $file +"normal $(echo $line)Gzz"
 end
 
 function find_run
