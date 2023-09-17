10d8f5a2a9e3b5d80c834970c3de53f0be7e5743
Shidil Eringa <shidil@live.com>
2023-09-17 16:39:13 +1000

    fix: removed telescope-zoxide keybinding

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -10,7 +10,7 @@ vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<leader>pF', builtin.git_files, {})
 vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>po', builtin.oldfiles, {})
-vim.keymap.set('n', '<leader>pp', require("telescope").extensions.zoxide.list, {})
+-- vim.keymap.set('n', '<leader>pp', require("telescope").extensions.zoxide.list, {})
 vim.keymap.set('n', '<leader>ps', function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
