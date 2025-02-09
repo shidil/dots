3392f191bce9da96c385b098d78e35035834332f
Shidil Eringa <4880359+shidil@users.noreply.github.com>
2025-02-09 14:28:52 +1100

    chore: upgrade nvim configuration; remove lsp-zero

--- .config/nvim/lua/config/packer.lua
+++ .config/nvim/lua/config/packer.lua
@@ -18,7 +18,6 @@ require("lazy").setup({
         tag = '0.1.8',
         dependencies = { 'nvim-lua/plenary.nvim' }
     },
-    -- { "jvgrootveld/telescope-zoxide" },
     {
         "catppuccin/nvim",
         name = "catppuccin",
@@ -34,35 +33,194 @@ require("lazy").setup({
     { "nvim-treesitter/nvim-treesitter",            name = "treesitter", build = ":TSUpdate" },
     { "nvim-treesitter/nvim-treesitter-textobjects" },
     -- { "nvim-treesitter/playground" },
-    -- https://github.com/ThePrimeagen/harpoon/pull/321
-    -- { "ThePrimeagen/harpoon",                       event = "VeryLazy" },
     { "mbbill/undotree" },
-    -- { "tpope/vim-fugitive" },
     {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v3.x',
-        dependencies = {
-            -- LSP Support
-            { 'neovim/nvim-lspconfig' },
-            { 'williamboman/mason.nvim' },
-            { 'williamboman/mason-lspconfig.nvim' },
+        'williamboman/mason.nvim',
+        lazy = false,
+        opts = {},
+    },
 
-            -- Autocompletion
-            { 'hrsh7th/nvim-cmp' },     -- Required
-            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+    -- Autocompletion
+    {
+        'hrsh7th/nvim-cmp',
+        event = 'InsertEnter',
+        config = function()
+            local cmp = require('cmp')
+            require('luasnip.loaders.from_vscode').lazy_load()
+
+            cmp.setup({
+                sources = {
+                    { name = 'nvim_lsp' },
+                    { name = 'luasnip' },
+                    { name = 'buffer' },
+                },
+                mapping = cmp.mapping.preset.insert({
+                    -- scroll up and down the documentation window
+                    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+                    ['<C-d>'] = cmp.mapping.scroll_docs(4),
+                    ['<C-Space>'] = cmp.mapping.confirm({ select = true }),
+                }),
+                preselect = 'item',
+                completion = {
+                    completeopt = 'menu,menuone,noinsert',
+                },
+                snippet = {
+                    expand = function(args)
+                        --vim.snippet.expand(args.body)
+                        require('luasnip').lsp_expand(args.body)
+                    end,
+                },
+                window = {
+                    completion = cmp.config.window.bordered(),
+                    documentation = cmp.config.window.bordered(),
+                },
+                formatting = {
+                    -- changing the order of fields so the icon is the first
+                    fields = { 'menu', 'abbr', 'kind' },
+
+                    -- here is where the change happens
+                    format = function(entry, item)
+                        local menu_icon = {
+                            nvim_lsp = 'λ',
+                            luasnip = '⋗',
+                            buffer = '',
+                            path = '',
+                            nvim_lua = 'Π',
+                        }
+
+                        item.menu = menu_icon[entry.source.name]
+                        return item
+                    end,
+                },
+            })
+        end,
+        dependencies = {
             {
+                "hrsh7th/cmp-buffer",
                 "L3MON4D3/LuaSnip",
                 dependencies = { "rafamadriz/friendly-snippets" },
-            },
-            { 'saadparwaiz1/cmp_luasnip' },
-            { 'hrsh7th/cmp-buffer' },
-            {
-                'lewis6991/gitsigns.nvim',
-                opts = function()
-                    require('gitsigns').setup()
-                end
-            },
+            }
         }
+    },
+    -- LSP
+    {
+        'neovim/nvim-lspconfig',
+        cmd = { 'LspInfo', 'LspInstall', 'LspStart' },
+        event = { 'BufReadPre', 'BufNewFile' },
+        dependencies = {
+            { 'hrsh7th/cmp-nvim-lsp' },
+            { 'williamboman/mason.nvim' },
+            { 'williamboman/mason-lspconfig.nvim' },
+        },
+        init = function()
+            -- Reserve a space in the gutter
+            -- This will avoid an annoying layout shift in the screen
+            vim.opt.signcolumn = 'yes'
+        end,
+        config = function()
+            local lsp_defaults = require('lspconfig').util.default_config
+
+            -- Add cmp_nvim_lsp capabilities settings to lspconfig
+            -- This should be executed before you configure any language server
+            lsp_defaults.capabilities = vim.tbl_deep_extend(
+                'force',
+                lsp_defaults.capabilities,
+                require('cmp_nvim_lsp').default_capabilities()
+            )
+
+            -- https://lsp-zero.netlify.app/docs/language-server-configuration.html#diagnostics
+            vim.diagnostic.config({
+                signs = {
+                    text = {
+                        [vim.diagnostic.severity.ERROR] = '✘',
+                        [vim.diagnostic.severity.WARN] = '▲',
+                        [vim.diagnostic.severity.HINT] = '⚑',
+                        [vim.diagnostic.severity.INFO] = '»',
+                    },
+                },
+            })
+
+            -- https://lsp-zero.netlify.app/docs/language-server-configuration.html#enable-format-on-save
+            local buffer_autoformat = function(bufnr)
+                local group = 'lsp_autoformat'
+                vim.api.nvim_create_augroup(group, { clear = false })
+                vim.api.nvim_clear_autocmds({ group = group, buffer = bufnr })
+
+                vim.api.nvim_create_autocmd('BufWritePre', {
+                    buffer = bufnr,
+                    group = group,
+                    desc = 'LSP format on save',
+                    callback = function()
+                        -- note: do not enable async formatting
+                        vim.lsp.buf.format({ async = false, timeout_ms = 10000 })
+                    end,
+                })
+            end
+            vim.api.nvim_create_autocmd('LspAttach', {
+                desc = 'Buf Format on Save',
+                callback = function(event)
+                    local id = vim.tbl_get(event, 'data', 'client_id')
+                    local client = id and vim.lsp.get_client_by_id(id)
+                    if client == nil then
+                        return
+                    end
+
+                    -- make sure there is at least one client with formatting capabilities
+                    if client.supports_method('textDocument/formatting') then
+                        buffer_autoformat(event.buf)
+                    end
+                end
+            })
+
+            -- LspAttach is where you enable features that only work
+            -- if there is a language server active in the file
+            vim.api.nvim_create_autocmd('LspAttach', {
+                desc = 'LSP actions',
+                callback = function(event)
+                    local opts = { buffer = event.buf }
+
+                    vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+                    vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+                    vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+                    vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+                    vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+                    vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+                    vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+                    vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+                    vim.keymap.set({ 'n', 'x' }, '<F3>',
+                        '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+                    vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+                    vim.keymap.set('n', 'ga', vim.lsp.buf.code_action)
+                    vim.keymap.set('n', 'gA', function()
+                        vim.lsp.buf.code_action({
+                            context = {
+                                only = {
+                                    "source",
+                                },
+                                diagnostics = {},
+                            },
+                        })
+                    end)
+                end,
+            })
+            require('mason-lspconfig').setup({
+                ensure_installed = {},
+                handlers = {
+                    -- this first function is the "default handler"
+                    -- it applies to every language server without a "custom handler"
+                    function(server_name)
+                        require('lspconfig')[server_name].setup({})
+                    end,
+                }
+            })
+        end
+    },
+    {
+        "TabbyML/vim-tabby",
+        lazy = false,
+        dependencies = {
+            "neovim/nvim-lspconfig",
+        },
     },
     {
         "nvim-tree/nvim-tree.lua",
@@ -79,7 +237,13 @@ require("lazy").setup({
             end)
         end,
     },
-    { "lukas-reineke/indent-blankline.nvim", event = "VeryLazy" },
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        event = "VeryLazy",
+        config = function()
+            require("ibl").setup()
+        end
+    },
     -- session
     {
         "rmagatti/auto-session",
@@ -146,16 +310,6 @@ require("lazy").setup({
             require("mini.pairs").setup(opts)
         end,
     },
-    --
-    {
-        'akinsho/bufferline.nvim',
-        version = "v4.*",
-        enabled = false,
-        dependencies = 'nvim-tree/nvim-web-devicons',
-        config = function(_, _)
-            require("bufferline").setup()
-        end
-    },
     -- surround
     {
         "echasnovski/mini.surround",
@@ -201,6 +355,16 @@ require("lazy").setup({
             require('leap').add_default_mappings()
         end
     },
-    { "mg979/vim-visual-multi",              event = "VeryLazy" },
-    { 'TabbyML/vim-tabby' }
+    { "mg979/vim-visual-multi", event = "VeryLazy" },
+    {
+        'romgrk/barbar.nvim',
+        init = function() vim.g.barbar_auto_setup = false end,
+        opts = {
+            -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
+            -- animation = true,
+            -- insert_at_start = true,
+            -- …etc.
+        },
+        version = '^1.9.1', -- optional: only update when a new 1.x version is released
+    },
 })
--- .config/nvim/lua/config/remaps.lua
+++ .config/nvim/lua/config/remaps.lua
@@ -36,5 +36,59 @@ vim.keymap.set("v", "<Tab>", ">><Esc>gv")
 vim.keymap.set("v", "<S-Tab>", "<<<Esc>gv")
 
 -- Buffers
-vim.keymap.set("n", "<leader>bc", ":bd ")
+--vim.keymap.set("n", "<leader>bc", ":bd ")
 vim.keymap.set("t", "<esc><esc>", "<C-\\><C-N>")
+
+local map = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+
+-- Move to previous/next
+map('n', '<leader>]', '<Cmd>BufferPrevious<CR>', opts)
+map('n', '<leader>[', '<Cmd>BufferNext<CR>', opts)
+
+-- Re-order to previous/next
+map('n', '<leader>}', '<Cmd>BufferMovePrevious<CR>', opts)
+map('n', '<leader>{', '<Cmd>BufferMoveNext<CR>', opts)
+
+-- Goto buffer in position...
+map('n', '<leader>1', '<Cmd>BufferGoto 1<CR>', opts)
+map('n', '<leader>2', '<Cmd>BufferGoto 2<CR>', opts)
+map('n', '<leader>3', '<Cmd>BufferGoto 3<CR>', opts)
+map('n', '<leader>4', '<Cmd>BufferGoto 4<CR>', opts)
+map('n', '<leader>5', '<Cmd>BufferGoto 5<CR>', opts)
+map('n', '<leader>6', '<Cmd>BufferGoto 6<CR>', opts)
+map('n', '<leader>7', '<Cmd>BufferGoto 7<CR>', opts)
+map('n', '<leader>8', '<Cmd>BufferGoto 8<CR>', opts)
+map('n', '<leader>9', '<Cmd>BufferGoto 9<CR>', opts)
+map('n', '<leader>0', '<Cmd>BufferLast<CR>', opts)
+
+-- Pin/unpin buffer
+map('n', '<leader>bp', '<Cmd>BufferPin<CR>', opts)
+
+-- Goto pinned/unpinned buffer
+--                 :BufferGotoPinned
+--                 :BufferGotoUnpinned
+
+-- Close buffer
+map('n', '<leader>bc', '<Cmd>BufferClose<CR>', opts)
+
+-- Wipeout buffer
+--                 :BufferWipeout
+
+-- Close commands
+--                 :BufferCloseAllButCurrent
+--                 :BufferCloseAllButPinned
+--                 :BufferCloseAllButCurrentOrPinned
+--                 :BufferCloseBuffersLeft
+--                 :BufferCloseBuffersRight
+
+-- Magic buffer-picking mode
+map('n', '<leader>g', '<Cmd>BufferPick<CR>', opts)
+map('n', '<leader>G', '<Cmd>BufferPickDelete<CR>', opts)
+
+-- Sort automatically by...
+map('n', '<leader>bb', '<Cmd>BufferOrderByBufferNumber<CR>', opts)
+map('n', '<leader>bn', '<Cmd>BufferOrderByName<CR>', opts)
+map('n', '<leader>bd', '<Cmd>BufferOrderByDirectory<CR>', opts)
+map('n', '<leader>bl', '<Cmd>BufferOrderByLanguage<CR>', opts)
+map('n', '<leader>bw', '<Cmd>BufferOrderByWindowNumber<CR>', opts)
