a6d50430735008921c26441e3867304a58b773c3
Shidil Eringa <4880359+shidil@users.noreply.github.com>
2025-04-29 09:40:03 +1000

    chore: added missing nvim lsp autoformat config

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -48,3 +48,33 @@ vim.api.nvim_create_autocmd('LspAttach', {
     end)
   end,
 })
+local buffer_autoformat = function(bufnr)
+  local group = 'lsp_autoformat'
+  vim.api.nvim_create_augroup(group, { clear = false })
+  vim.api.nvim_clear_autocmds({ group = group, buffer = bufnr })
+
+  vim.api.nvim_create_autocmd('BufWritePre', {
+    buffer = bufnr,
+    group = group,
+    desc = 'LSP format on save',
+    callback = function()
+      -- note: do not enable async formatting
+      vim.lsp.buf.format({ async = false, timeout_ms = 10000 })
+    end,
+  })
+end
+vim.api.nvim_create_autocmd('LspAttach', {
+  desc = 'Buf Format on Save',
+  callback = function(event)
+    local id = vim.tbl_get(event, 'data', 'client_id')
+    local client = id and vim.lsp.get_client_by_id(id)
+    if client == nil then
+      return
+    end
+
+    -- make sure there is at least one client with formatting capabilities
+    if client.supports_method('textDocument/formatting') then
+      buffer_autoformat(event.buf)
+    end
+  end
+})
