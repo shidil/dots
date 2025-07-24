e57838c5aebdabac7827e64699a57989519655e2
Shidil Eringa <4880359+shidil@users.noreply.github.com>
2025-07-24 12:31:50 +1000

    chore: improved wezterm tab bar appearance

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -8,19 +8,35 @@ config.font = wezterm.font("Hack Nerd Font")
 config.font_size = 12.0
 config.front_end = "WebGpu"
 config.webgpu_power_preference = "HighPerformance"
+config.max_fps = 144
+config.tiling_desktop_environments = {
+  "Wayland",
+}
 
-function scheme_for_appearance(appearance)
-	if appearance:find "Dark" then
-		return "Catppuccin Mocha"
-	else
-		return "Catppuccin Mocha"
-	end
+local function scheme_for_appearance(appearance)
+  if appearance:find "Dark" then
+    return "Catppuccin Mocha"
+  else
+    return "Catppuccin Mocha"
+  end
 end
 
-config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
+local scheme = scheme_for_appearance(wezterm.gui.get_appearance())
+local custom = wezterm.color.get_builtin_schemes()[scheme]
 
-config.window_decorations = "RESIZE"
-config.enable_tab_bar = false
+config.color_schemes = {
+  [scheme] = custom,
+}
+
+config.color_scheme = scheme
+
+config.window_decorations = "NONE"
+config.enable_tab_bar = true
+config.tab_bar_at_bottom = false
+config.tab_max_width = 32
+config.use_fancy_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
+
 
 config.window_background_opacity = 0.95 -- Default opacity when not fullscreen
 config.macos_window_background_blur = 8
